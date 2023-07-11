9c806c0554c7c48bd053c46b9bcddbfd6a25c5b3
smartkobieta <allavovk39@gmail.com>
2023-07-11 17:39:18 +0200

    JS

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,15 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn1: document.querySelector('[data-modal-open1]'),
+    openModalBtn2: document.querySelector('[data-modal-open2]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal1);
+  refs.openModalBtn2.addEventListener('click', toggleModal2);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
