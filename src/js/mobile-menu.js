3881b384e6c080606a507587fd79a386bb561bd9
smartkobieta <allavovk39@gmail.com>
2023-07-13 11:34:57 +0200

    mob.men

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,12 +2,14 @@
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
+    closeMenuBtn1: document.querySelector('[data-menu-close1]'),
 
     menu: document.querySelector('[data-menu]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn1.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
