65fdafb132ab5d9e91b2777489339dd66dab2e9d
RomanMytsu <132835114+RomanMytsu@users.noreply.github.com>
2023-07-13 16:14:02 +0300

    Scroll-up

---
+++ src/js/scroll-up.js
@@ -0,0 +1,24 @@
+let mybutton = document.getElementById('btn-back-to-top');
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    mybutton.classList.add('show');
+  } else {
+    mybutton.classList.remove('show');
+  }
+}
+
+mybutton.addEventListener('click', backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  mybutton.blur();
+}
